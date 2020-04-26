fe35abf7d7c1226d48eeaf5d503ea5cfd9ee4812
lior300 <lior_g@info.cts.org.il>
2020-04-26 21:05:25 +0300

    liorShaharDay1

--- apps/MissKeep/cmps/Note.jsx
+++ apps/MissKeep/cmps/Note.jsx
@@ -5,7 +5,7 @@ export default class Note extends React.Component {
     render() {
         const { id, type, isPinned, info, style } = this.props.note
         return (
-            <article>
+            <article style={style} className="note">
                 <NoteContent type={type} info={info} key={id} />
             </article>
         )
