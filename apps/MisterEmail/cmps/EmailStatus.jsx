6b3706f7cc9f15319d98e53b86ca57ae82774c5f
shahar <shaharp2@gmail.com>
2020-04-29 20:02:47 +0300

    changeImg

--- apps/MisterEmail/cmps/EmailStatus.jsx
+++ apps/MisterEmail/cmps/EmailStatus.jsx
@@ -3,16 +3,10 @@ import { eventBus } from '../../../services/eventBusService.js'
 export default function EmailStatus(props) {
 
     function showStatus() {
-        // var mails = this.props.mails;
         const readingMails = props.mails.filter(mail => !mail.isDelete && mail.isRead)
-
         if(props.mails.length != 0){
-        // const readingMails = mails.filter(mail => mail.isRead === true)
         var precent = parseInt((readingMails.length / props.mails.length) * 100);
         }else precent = 0
-
-        
-
         return precent
     }
 
